1eb25b9085622adbf8948b7327b101ca75b8ad2b
Muhammad Mardhotillah <muhammadmardhatila15@gmail.com>
2024-07-22 09:04:46 +0800

    chart ok

--- resources/views/chart/dashboard.blade.php
+++ resources/views/chart/dashboard.blade.php
@@ -3,168 +3,195 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet"/>
+    <title>Dashboard Overview</title>
+    <!-- Boxicon -->
+     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+     <!-- CSS -->
     <link rel="stylesheet" href="/css.css">
-    <title>Dashboard</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
-    <script src="/js.js"></script>
+    <!-- JS -->
+     <script src="/public/js.js"></script>
 </head>
 <body>
-     <div class="sidebar">
-        <img src="/images/IMG/Group 10.png" alt="" style="width: 120px; margin-top: 30px; margin-left: 20px;">
-        <ul>
-            <li><a href="#dashboard">
-                <img src="/images/IMG/Vector.png" alt="" style="width: 16px; padding-right: 5px; margin-top: 2px;">
-                Dashboard</a></li>
-            <li><a href="#reports">
-                <img src="/images/IMG/Vector2.png" alt="" style="width: 16px; padding-right: 5px; margin-top: 2px;">
-                Reports</a></li>
-            <li><a href="#about">
-                <img src="/images/IMG/Vector4.png" alt="" style="width: 16px; padding-right: 5px; margin-top: 2px;">
-                About</a></li>
-            <li><a href="#">
-                <img src="/images/IMG/Vector5.png" alt="" style="width: 16px; padding-right: 5px; margin-top: 2px;">
-                Logout</a></li>
-        </ul>
-     </div>
-     <div class="content">
-        <section id="dashboard">
-            <!-- Dashboard -->
-           <div class="dashboard">
-            <h1>Dashboard</h1>
-            <!-- Filter -->
-            <div class="filter">
-                <select id="Laman">
-                    <option value="Reagen">Reagen</option>
-                    <option value="Simantan">Simantan</option>
-                    <option value="E-Tamu">E-Tamu</option>
-                    <option value="Kepegawaian">Kepegawaian</option>
-                    <option value="Kearsipan">Kearsipan</option>
-                    <option value="Pengujian Sampel PK3">Pengujian Sampel PK3</option>
-                </select>
-                <button>Filter</button>
-                <button>Reset</button>
-            </div>
-            <!-- End Filter -->
-            <!-- Scorecard -->
-            <div class="summary">
-                <div class="summary-item">
-                    <img src="/images/IMG/Group 17.png" alt="stok" class="summary-icon">
-                    <p id="jmlstok">0</p>
-                    <h3>Jumlah Stok</h3>
-                </div>
-                <div class="summary-item">
-                    <img src="/images/IMG/Group 18.png" alt="stok" class="summary-icon">
-                    <p id="jmlreagen">0</p>
-                    <h3>Jumlah Reagen Keluar</h3>
-                </div>
-                <!-- Grafik -->
-               <div class="grafik-item">
-                 <h3>Presentase Jumlah permintaan</h3>
-                 <canvas id="presentase"></canvas>
-               </div>
-               <div class="grafik-item">
-                 <h3>Grafik Presentase permintaan Reagen</h3>
-                 <canvas id="grafik"></canvas>
-               </div>
-              <!-- End Grafik -->
-            </div>
-             <!-- End Scorecard -->
-           </div>
-      <!-- End Dashboard -->
+      <!-- Sidebar -->
+        <section id="sidebar">
+            <img src="/images/IMG/Group 10.png" alt="" class="head" style="width: 120px; margin-top: 20px; margin-left: 60px;">
+            <ul class="sidebar-menu">
+                <li class="active">
+                    <a href="#percepat">
+                        <i class='bx bxs-zap'></i>
+                        <span class="title">PERCEPAT</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#simantan">
+                        <i class='bx bxs-cog'></i>
+                        <span class="title">SIMANTAN</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#tamu">
+                        <i class='bx bx-fingerprint'></i>
+                        <span class="title">E-TAMU</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#kepegawaian">
+                        <i class='bx bxs-network-chart'></i>
+                        <span class="title">Kepegawaian</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#kearsipan">
+                        <i class='bx bx-book-alt'></i>
+                        <span class="title">Kearsipan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#pengujian">
+                        <i class='bx bx-atom'></i>
+                        <span class="title">Pengujian PK3</span>
+                    </a>
+                </li>
+            </ul>
+            <script>
+                const allSideMenu = document.querySelectorAll('#sidebar .sidebar-menu li a');
+
+                allSideMenu.forEach(item=> {
+                const li = item.parentElement;
+
+                item.addEventListener('click', function () {
+                    allSideMenu.forEach(i=> {
+                        i.parentElement.classList.remove('active');
+                    })
+                    li.classList.add('active');
+                })
+                });
+            </script>
         </section>
-        <section id="reports">
-      <!-- Reports -->
-           <div class="reports">
-                <h1>Reports</h1>
-                <div class="table">
-                    <table id="pizzaTable" class="display" style="width: 100%">
-                   <thead>
-                       <tr>
-                           <th>Name</th>
-                           <th>Category</th>
-                           <th>Ingredients</th>
-                           <th>Date</th>
-                           <th>Time</th>
-                           <th>Price</th>
-                           <th>Quantity</th>
-                           <th>Size</th>
-                       </tr>
-                   </thead>
-                   <tbody></tbody>
-               </table>
+      <!-- End Sidebar -->
+       <!-- Content -->
+        <section id="content">
+            <!-- NAV -->
+                <nav>
+                    <i class='bx bx-menu' ></i>
+                    <a href="#" class="tittle">
+                        <i class='bx bx-bullseye'></i>
+                        <span class="text">Dashboard Overview</span>
+                    </a>
+                    <img src="/images/IMG/Group 16.png" alt="" width="25px">
+                    <script>
+                        const menuBar = document.querySelector('#content nav .bx.bx-menu');
+                        const sidebar = document.getElementById('sidebar');
+
+                        menuBar.addEventListener('click', function () {
+                            sidebar.classList.toggle('hide');
+                        })
+                    </script>
+                </nav>
+            <!--END NAV -->
+            <!-- MAIN -->
+                <section id="percepat">
+                    <span class="teks">Percepat</span>
+                    <div class="wrapper">
+                        <div class="summary">
+                            <div class="summary-item">
+                                <img src="/images/IMG/Group 17.png" alt="stok" class="summary-icon" style="width: 30px; height: 30px;">
+                                <p id="jmlstok">0</p>
+                                <h3>Jumlah Stok</h3>
+                            </div>
+                            <div class="summary-item">
+                                <img src="/images/IMG/Group 18.png" alt="stok" class="summary-icon" style="width: 30px; height: 30px;">
+                                <p id="jmlstok">0</p>
+                                <h3>Jumlah Reagen Keluar</h3>
+                            </div>
+                        </div>
+                        <div class="grafik-item">
+                            <h3>Grafik Permintaan</h3>
+                            <canvas id="permintaan"></canvas>
+                          </div>
+                          <div class="grafik-item">
+                            <h3>Diagram Jumlah Reagen</h3>
+                            <canvas id="grafik"></canvas>
+                          </div>
                     </div>
-                   <!-- End Tables -->
-                    <!-- Document Order -->
-                     <h4>.Insight Document</h4>
-                    <div class="document-wrapper">
-                       <div class="document">
-                           <h3>Reagen</h3>
-                           <img src="/images/IMG/doc.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>
-                       <div class="document">
-                           <h3>SIMANTAN</h3>
-                           <img src="/images/IMG/doc2.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>            
-                       <div class="document">
-                           <h3>E-Tamu</h3>
-                           <img src="/images/IMG/doc3.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>            
-                       <div class="document">
-                           <h3>Kepegawaian</h3>
-                           <img src="/images/IMG/doc.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>            <div class="document">
-                           <h3>Kearsipan</h3>
-                           <img src="/images/IMG/doc2.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>
-                       <div class="document">
-                           <h3>Pengujian Sampel PK3</h3>
-                           <img src="/images/IMG/doc3.png" alt="">
-                           <button>
-                               <img src="/images/IMG/down.png" alt="">
-                               <a href="">Download</a>
-                           </button>
-                       </div>
-                     </div>
-                    <!-- End Document Order -->
-           </div>
-      <!-- End Reports  -->
+                </section>
+                <section id="simantan">
+                    <span class="teks">SIMANTAN</span>
+                    <div class="wrapper">
+                        <div class="simantan">
+                            <div class="grafik-item">
+                                <h3>Tata Usaha</h3>
+                                <canvas id="tatausaha"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Informasi & Komunikasi</h3>
+                                <canvas id="inforkom"></canvas>
+                            </div> 
+                            <div class="grafik-item">
+                                <h3>Pemeriksaan</h3>
+                                <canvas id="pemeriksaan"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Penindakan</h3>
+                                <canvas id="penindakan"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Pengujian</h3>
+                                <canvas id="pengujian"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="tamu">
+                    <span class="teks">E-Tamu</span>
+                    <div class="wrapper">
+                        <div class="e-tamu">
+                            <div class="grafik-item">
+                                <h3>Pengujian Barang Narkoba</h3>
+                                <canvas id="narkoba"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Pengujian Obat & Makanan</h3>
+                                <canvas id="pom"></canvas>
+                            </div> 
+                            <div class="grafik-item">
+                                <h3>Informasi & Pengaduan</h3>
+                                <canvas id="pengaduan"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Sertifikasi</h3>
+                                <canvas id="sertif"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Wajib Lapor</h3>
+                                <canvas id="lapor"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Kunjungan</h3>
+                                <canvas id="kunjungan"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Keperluan Pribadi</h3>
+                                <canvas id="pribadi"></canvas>
+                            </div>
+                            <div class="grafik-item">
+                                <h3>Keperluan Lain</h3>
+                                <canvas id="lain"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="kepegawaian">
+                    <span class="teks">Kepegawaian</span>
+                </section>
+                <section id="kearsipan">
+                    <span class="teks">Kearsipan</span>
+                </section>
+                <section id="pengujian">
+                    <span class="teks">Pengujian Sampel PK3</span>
+                </section>
+            <!-- END MAIN -->
         </section>
-        <section id="about">
-            <!-- About -->
-           <div class="about">
-             <div class="about-content">
-                <a href="#home"><img src="/images/IMG/Group 10.png" alt="" style="width: 100px;"></a>
-                <p> Balai Besar POM 2024 <br> </p>
-                <h2>Copyright &copy;BBPOM Mataram 2024</h2>
-             </div>
-           </div>
-      <!-- End About -->
-        </section>
-    </div>
+       <!-- End Content -->
 </body>
 </html>
